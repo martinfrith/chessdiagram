1779753551d8f516d4882b56d4a4853b197f38d2
martin <telemgico@gmail.com>
2020-01-18 11:39:26 -0300

    cache control

--- server.js
+++ server.js
@@ -5,21 +5,21 @@ var bodyParser = require('body-parser')
 var compression = require('compression')
 var app = express()
 
-
 app.use(compression())
 app.use(bodyParser.json())
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  //res.header("Cache-Control", "public, max-age=2592000")
-  //res.removeHeader('Cache-Control')
-  res.header("Expires", new Date(Date.now() + 2592000000).toUTCString())
-  next()
+	res.header("Access-Control-Allow-Origin", "*")
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+
+	if(req.url.indexOf('/img/') === 0 || req.url.indexOf('/dist/') === 0){
+		res.header("Expires", new Date(Date.now() + 2592000000).toUTCString())
+	}
+	next()
 })
 
 app.use(express.static(__dirname + '/dist',{
-  maxAge: "1d"
+	maxAge: "1d"
 }))
 
 app.get('*', function(req, res){
@@ -29,5 +29,5 @@ app.get('*', function(req, res){
 app.set('port', process.env.PORT || 8000)
 
 var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening at http://localhost:' + server.address().port)
+	console.log('Express server listening at http://localhost:' + server.address().port)
 })
