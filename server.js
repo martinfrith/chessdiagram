5ed249aa4ad3e83a28cce027957c15a47ecaeeb0
martin <telemgico@gmail.com>
2020-01-18 11:31:48 -0300

    og:image strange fix

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   //res.header("Cache-Control", "public, max-age=2592000")
+  //res.removeHeader('Cache-Control')
   res.header("Expires", new Date(Date.now() + 2592000000).toUTCString())
   next()
 })
